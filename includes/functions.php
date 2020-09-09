96ad2247d15bf65f30287491e92df52ae2f0175c
Dev Buntoo <haroon.abid.1999@gmail.com>
2020-09-09 18:13:49 +0500

    Add to cart button working perfectly

--- includes/functions.php
+++ includes/functions.php
@@ -144,8 +144,98 @@ if(isset($_POST['remove_fav'])){
   }
 }
 
+//This function add item to cart
+
+if(isset($_POST['add_to_cart'])){
+  $pro_id = $_POST['add_to_cart'];
+
+  //geting store id of inserting product
+  $get_store_id_sql = "SELECT store_id FROM product_tbl WHERE id = '$pro_id'";
+  $run_to_get_store_id = mysqli_fetch_array(mysqli_query($conn, $get_store_id_sql));
+  $store_id = $run_to_get_store_id['store_id'];
+
+  //logged in session id
+  $user_id = $login_session_id;
+
+  /*
+  check if the unchecked order is present for the current user and product store
+  */
+  $check_for_unchecked_order = "SELECT * FROM order_tbl WHERE user_id = '$user_id' && store_id = '$store_id' && status = 'unchecked'";
+  $run_to_check_uncheck_order = mysqli_query($conn, $check_for_unchecked_order);
+  $order_tbl_row = mysqli_fetch_array($run_to_check_uncheck_order);
+  $order_tbl_check_count = mysqli_num_rows($run_to_check_uncheck_order);
+  if($order_tbl_check_count == 1){
+   //echo "<script> window.alert('order found');</script>";
+   //if found check if the item is already in cart
+   $order_id = $order_tbl_row['id'];
+   $check_item_sql = "SELECT * FROM cart_tbl WHERE pro_id = '$pro_id' && order_id = '$order_id'";
+   $check_item_run = mysqli_num_rows(mysqli_query($conn, $check_item_sql));
+   if($check_item_run == 0){
+    //echo "<script> window.alert('Item not found');</script>";
+     //Adding item to cart
+     $add_item_to_cart_sql = "INSERT INTO cart_tbl (order_id, pro_id) VALUES ('$order_id', '$pro_id')";
 
 
+     if($conn -> query($add_item_to_cart_sql) === TRUE){
+       //if success
+       echo "<script> window.alert('Item Added to Cart');</script>
+       <script type='text/javascript'>window.open('','_self')</script>";
+     }
+     else{
+ 
+       //show error if add to cart failed
+       echo "Error: " . $add_item_to_cart_sql . "<br>" . $conn->error;
+     }
+   }
+   else{
+    echo "<script> window.alert('Item Already In Cart');</script>";
+   }
+   
+
+
+  }
+  else{
+   // echo "<script> window.alert('order not found');</script>";
+
+
+   //order not found so adding a new order
+   $add_new_order_sql = "INSERT INTO order_tbl (user_id, store_id, status, status_description) VALUES ('$user_id', '$store_id', 'unchecked', 'Proced to check out.')";
+   if( $conn -> query($add_new_order_sql) === TRUE ){
+
+
+     //If new order created successfuly then get the new order id
+    $new_order_id = mysqli_insert_id($conn);
+
+
+
+    //Adding item to cart
+    $add_item_to_cart_sql = "INSERT INTO cart_tbl (order_id, pro_id) VALUES ('$new_order_id', '$pro_id')";
+
+
+    if($conn -> query($add_item_to_cart_sql) === TRUE){
+      //if success
+      echo "<script> window.alert('Item Added to Cart');</script>
+      <script type='text/javascript'>window.open('','_self')</script>";
+    }
+    else{
+
+      //show error if add to cart failed
+      echo "Error: " . $add_item_to_cart_sql . "<br>" . $conn->error;
+    }
+
+
+
+
+   }
+   else{
+     //show error if creating order failed
+    echo "Error: " . $add_new_order_sql . "<br>" . $conn->error;
+   }
+
+  }
+
+
+}
 
 
 
